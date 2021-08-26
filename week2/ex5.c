0c17fe0aa0153da996481292700d356a36610b7f
V_Roman_V <vor.roman.v@gmail.com>
2021-08-26 18:13:52 +0300

    Update exercise 5

--- week2/ex5.c
+++ week2/ex5.c
@@ -35,6 +35,31 @@ void figure3(int n){
 	}
 }
 
+void intro(){
+	int n = 5;
+	for(int i = 0; i<n; i++){
+		// 1
+		printf((i==0)?"1) ":"   ");
+		duplicate(' ', n - i - 1);
+		duplicate('*', 1 + 2*i);
+		duplicate(' ', n - i - 1);
+		// 2
+		printf((i==0)?" 2) ":"    ");
+		duplicate('*', 1 + i);
+		duplicate(' ', n - i - 1);
+		putchar(' ');
+		// 3
+		printf((i==0)?" 3) ":"    ");
+		duplicate('*', 1 + n/2 - abs(n/2 - i));
+		duplicate(' ', 1 + abs(n/2 - i));
+		putchar(' ');
+		// 4
+		printf((i==0)?" 4) ":"    ");
+		duplicate('*', n);
+		putchar('\n');
+	}
+}
+
 int readInt(){
 	char num[32];
 	if(fgets(num, 32, stdin) == NULL){
@@ -53,12 +78,15 @@ int main(void) {
 	fig[3] = &figure3;
 
 	printf("Enter the number of the figure [1;4]:\n");
+	intro();
+	putchar('>');
 	int num = readInt();
 	if (num < 1 || 4 < num){
 		printf("Incorrect number:\n");
 		return 0;
 	}
 	printf("Enter the height of the figure:\n");
+	putchar('>');
 	int h = readInt();
 	fig[num-1](h);
 	return 0;
