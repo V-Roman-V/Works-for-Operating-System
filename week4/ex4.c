29899ff949ae72d3726c8157c20ba4fe945b54b6
V_Roman_V <vor.roman.v@gmail.com>
2021-09-20 21:31:55 +0300

    update the fourth task

--- week4/ex4.c
+++ week4/ex4.c
@@ -3,10 +3,14 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <string.h>
+#include <sys/wait.h>
+
 
 int main(){
 	char str[256];
 	while (1) {
+		putchar('>');
+    int status;
 		fgets(str, 256, stdin);
 		char* args[16];
 		args[0] = strtok(str," \n");
@@ -14,14 +18,19 @@ int main(){
 		for(;cnt < 15; cnt++)
 			if( (args[cnt] = strtok(NULL," \n")) == NULL)
 				break;
-		if (!strcmp(args[cnt-1], "&")){
-		    if (!fork()){
-		        args[cnt-1] = NULL;
-		        execvp(args[0], args);
-		        return 0;
-		    }
-		} else
-		    execvp(args[0], args);
+
+		char in_bg = !strcmp(args[cnt-1], "&");
+
+		if (!fork()){
+			if (in_bg)
+				args[cnt-1] = NULL;
+			execvp(args[0], args);
+			return 0;
+		}
+
+		sleep(1);
+		if (!in_bg)
+			waitpid(-1, &status, 0);
   }
   return 0;
 }
