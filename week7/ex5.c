01f9a911b32f5cd151aeda3eff7fef88ecf9e2db
V_Roman_V <vor.roman.v@gmail.com>
2021-09-30 17:46:10 +0300

    update the fifth task

--- week7/ex5.c
+++ week7/ex5.c
@@ -5,8 +5,8 @@ int main () {
 	char **s = (char**) malloc(sizeof(char*));
 	char foo[] = "Hello World";
 	*s = foo; // s[0] equal to *s
-	printf ("s is %s\n", s[0]);
+	printf ("s is %p\n", s);
 	s[0] = foo; 
-	printf ("s[0] is %c\n", s[0][0]);
+	printf ("s[0] is %s\n", s[0]);
 	return (0) ;
 }
